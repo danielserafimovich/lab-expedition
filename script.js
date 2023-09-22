189090dae7725f0ab56d88a54b6cfe4b05f59c1e
danielserafimovich <119928204+danielserafimovich@users.noreply.github.com>
2023-09-22 13:18:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,31 @@
-// Initialize PoseNet here and add code to draw a mustache on the canvas based on facial landmarks detected by PoseNet.
-// This code should include TensorFlow.js and PoseNet setup.
+document.addEventListener('DOMContentLoaded', () => {
+    // Access the startbutton element using document.getElementById
+    const startbutton = document.getElementById('startbutton');
 
-// Access the startbutton element using document.getElementById
-const startbutton = document.getElementById('startbutton');
-
-// Check if the startbutton element exists
-if (startbutton) {
-    startbutton.addEventListener('click', () => {
-        // Call a function to start facial detection and mustache drawing
-        startFaceDetection();
-    });
-} else {
-    console.error('startbutton element not found');
-}
-
-// Define a function to start facial detection
-async function startFaceDetection() {
-    const video = document.getElementById('video');
-    const canvas = document.getElementById('output');
-
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        video.srcObject = stream;
-        video.play();
-
-        const net = await setupPoseNet();
-
-        video.addEventListener('play', () => {
-            const canvasCtx = canvas.getContext('2d');
-            async function poseDetectionFrame() {
-                const pose = await net.estimateSinglePose(video);
-                canvasCtx.clearRect(0, 0, video.width, video.height);
-                canvasCtx.drawImage(video, 0, 0, video.width, video.height);
-                drawMustache(pose.keypoints); // Call the function to draw the mustache
-                requestAnimationFrame(poseDetectionFrame);
-            }
-            poseDetectionFrame();
+    // Check if the startbutton element exists
+    if (startbutton) {
+        startbutton.addEventListener('click', () => {
+            // Call a function to start facial detection and mustache drawing
+            startFaceDetection();
         });
-    } catch (error) {
-        console.error('Error accessing the camera:', error);
+    } else {
+        console.error('startbutton element not found');
     }
-}
 
-// Define a function to setup PoseNet
-async function setupPoseNet() {
-    // Your PoseNet setup code goes here.
-}
+    // Define a function to start facial detection
+    async function startFaceDetection() {
+        // Rest of your code for facial detection goes here
+    }
 
-// Define a function to draw a mustache based on facial landmarks
-function drawMustache(faceLandmarks) {
-    // Your code to draw a mustache on the canvas using the facial landmarks goes here.
-}
+    // Define a function to setup PoseNet
+    async function setupPoseNet() {
+        // Your PoseNet setup code goes here
+    }
+
+    // Define a function to draw a mustache based on facial landmarks
+    function drawMustache(faceLandmarks) {
+        // Your code to draw a mustache on the canvas using the facial landmarks goes here
+    }
+
+    // Call any other initialization functions here
+});
