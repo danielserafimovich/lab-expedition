cfa6e3cc75d130e558491e23d8e03a9738e99cf7
danielserafimovich <119928204+danielserafimovich@users.noreply.github.com>
2023-09-22 13:12:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,53 @@
-// Function to add green webbing and a mustache to the captured image
-async function addWebbingAndMustacheToImage(imageData) {
-    const imageElement = document.createElement('img');
-    imageElement.src = imageData;
+// Initialize PoseNet here and add code to draw a mustache on the canvas based on facial landmarks detected by PoseNet.
+// This code should include TensorFlow.js and PoseNet setup.
 
-    const pose = await net.estimateSinglePose(imageElement);
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
+// Access the startbutton element using document.getElementById
+const startbutton = document.getElementById('startbutton');
 
-    // Define the body part groups you want to outline
-    const bodyPartGroups = [
-        ['leftShoulder', 'leftElbow', 'leftWrist'],
-        ['rightShoulder', 'rightElbow', 'rightWrist'],
-        ['leftShoulder', 'rightShoulder'],
-        ['leftHip', 'rightHip'],
-        ['leftHip', 'leftKnee', 'leftAnkle'],
-        ['rightHip', 'rightKnee', 'rightAnkle'],
-        ['leftEye', 'rightEye'],
-        ['leftEar', 'rightEar'],
-    ];
-
-    context.drawImage(imageElement, 0, 0, canvas.width, canvas.height);
-
-    context.strokeStyle = 'green';
-    context.lineWidth = 2;
-
-    // Draw green lines around selected body part groups
-    bodyPartGroups.forEach(group => {
-        context.beginPath();
-        group.forEach(partName => {
-            const part = pose.keypoints.find(kp => kp.part === partName);
-            if (part) {
-                context.lineTo(part.position.x, part.position.y);
-            }
-        });
-        context.closePath();
-        context.stroke();
+// Check if the startbutton element exists
+if (startbutton) {
+    startbutton.addEventListener('click', () => {
+        // Call a function to start facial detection and mustache drawing
+        startFaceDetection();
     });
+} else {
+    console.error('startbutton element not found');
+}
 
-    // Add a mustache to the image (you can use your existing mustache-drawing code here)
-    if (pose.keypoints[0].score > 0.5) {
-        const noseX = pose.keypoints[0].position.x;
-        const noseY = pose.keypoints[0].position.y;
+// Define a function to start facial detection
+async function startFaceDetection() {
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('output');
 
-        // Calculate the position and size of the mustache (under the nose)
-        const mustacheWidth = 2 * pose.keypoints[0].position.x;
-        const mustacheHeight = mustacheImage.height * (mustacheWidth / mustacheImage.width);
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        video.srcObject = stream;
+        video.play();
 
-        // Draw the mustache image under the nose
-        context.drawImage(
-            mustacheImage,
-            noseX - mustacheWidth / 2,
-            noseY - mustacheHeight / 2,
-            mustacheWidth,
-            mustacheHeight
-        );
+        const net = await setupPoseNet();
+
+        video.addEventListener('play', () => {
+            const canvasCtx = canvas.getContext('2d');
+            async function poseDetectionFrame() {
+                const pose = await net.estimateSinglePose(video);
+                canvasCtx.clearRect(0, 0, video.width, video.height);
+                canvasCtx.drawImage(video, 0, 0, video.width, video.height);
+                drawMustache(pose.keypoints); // Call the function to draw the mustache
+                requestAnimationFrame(poseDetectionFrame);
+            }
+            poseDetectionFrame();
+        });
+    } catch (error) {
+        console.error('Error accessing the camera:', error);
     }
 }
 
-// Event listener to capture an image and add green webbing and a mustache
-startButton.addEventListener('click', () => {
-    const imageData = captureImage();
-    addWebbingAndMustacheToImage(imageData);
-});
+// Define a function to setup PoseNet
+async function setupPoseNet() {
+    // Your PoseNet setup code goes here.
+}
+
+// Define a function to draw a mustache based on facial landmarks
+function drawMustache(faceLandmarks) {
+    // Your code to draw a mustache on the canvas using the facial landmarks goes here.
+}
